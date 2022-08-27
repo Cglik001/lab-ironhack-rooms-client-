bec1ccd9447a64fcb42abd6e935655444ccab268
christian landsberger Cglik001@gmail.com <cglik001@gmail.com>
2022-08-27 02:13:56 -0300

    deploy client

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+import {useContext} from 'react'
+import {AuthContext} from '../contexts/authContext.js'
+
+const Navbar = () => {
+  const {isLoading, loggedInUser, logout} = useContext(AuthContext)
+  console.log(isLoading, loggedInUser, logout)
+
+
+  console.log(isLoading, loggedInUser)
+
+
+
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            {(!isLoading && loggedInUser.token !== '') ? (
+              <>
+              <li className="nav-item active">
+                <Link className="nav-link" to="/rooms/new">
+                  Create Room
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link onClick={() => logout()} className="nav-link" to="#">Logout</Link>
+              </li>
+              </>
+              ) : (
+              <li className="nav-item active">
+                <Link className="nav-link" to="/auth/login">
+                  Login
+                </Link>
+              </li>
+              )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import apiService from "../services/api.service"
+import RoomPost from '../components/RoomPost'
+import {AuthContext} from '../contexts/authContext.js'
+
+
+function Home() {
+
+  const [Loading, setLoading] = useState(true)
+  const [refresh, setRefresh] = useState(true)
+  const [roomPosts, setRoomPosts] = useState([])
+  const {isLoading, loggedInUser} = useContext(AuthContext)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const rooms = await apiService.getRooms()
+        setRoomPosts(rooms)
+        setLoading(false)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    fetchData()
+
+  }, [refresh])
+
+  const deleteRoom = async (id) => {
+    try {
+      await apiService.deleteRoom(id)
+      setRefresh(!refresh)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  if (Loading) {
+    return <p>Loading room posts ...</p>
+  }
+
+
+  return (
+    <div className="container mt-20 d-flex justify-content-center">
+      <div className="row">
+        {roomPosts.map(post => {
+          return <RoomPost
+            post={post}
+            key={post._id}
+            loggedInUser={loggedInUser}
+            deleteRoom={deleteRoom}/>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
